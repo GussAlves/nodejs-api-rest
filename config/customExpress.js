5762d5eab6e18c56116711899151af7dfb42568d
Gustavo Alves <50598580+GussAlves@users.noreply.github.com>
2022-03-01 00:10:47 -0300

    Realizando conexão com banco de dados Mysql

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const consign = require('consign');
-const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express();
 
-    app.use(express.urlencoded({extended: true}))
+    app.use(express.urlencoded({extended: true})) // x-urlencoded
     app.use(express.json())
 
     // assim realizamos 
