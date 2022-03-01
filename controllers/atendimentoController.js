446624d4941e389a8cefb886a766254e4e222427
Gustavo Alves <50598580+GussAlves@users.noreply.github.com>
2022-02-28 22:59:39 -0300

    Enviando dados para api realizar validação

--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -3,6 +3,8 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res
         .send('Você está na rota de atendimentos utilizando método GET'));
 
-    app.post('/atendimentos', (req, res) => res
-        .send('Você está na rota de atendimentos utilizando método POST'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos utilizando método POST')
+    })
 }
