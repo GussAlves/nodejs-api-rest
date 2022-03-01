cbdabdc2556c7fbebd9d9fdaf3940815e0436c0a
Gustavo Alves <50598580+GussAlves@users.noreply.github.com>
2022-03-01 20:49:38 -0300

    Criando tratamento de erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,33 @@
-    const moment = require('moment');
-    const conexao = require('../infraestrutura/conexao');
+const moment = require('moment');
+const conexao = require('../infraestrutura/conexao');
 
-    class Atendimento {
-        adiciona(atendimento, res) {
-            const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-            const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+class Atendimento {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco catacteres'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
             const atendimentoDatado = { ...atendimento, dataCriacao, data };
             const sql = 'INSERT INTO ATENDIMENTOS SET ?';
 
@@ -16,18 +39,19 @@
                 }
             });
         }
-
-        exibirTodos() {
-            const sql = 'select * from atendimentos';
-
-            conexao.query(sql, (erro, resultado) => {
-                if (erro) {
-                    console.log(erro);
-                } else {
-                    return resultado;
-                }
-            })
-        }
     }
 
-    module.exports = new Atendimento;
+    exibirTodos() {
+        const sql = 'select * from atendimentos';
+
+        conexao.query(sql, (erro, resultado) => {
+            if (erro) {
+                console.log(erro);
+            } else {
+                return resultado;
+            }
+        })
+    }
+}
+
+module.exports = new Atendimento;
